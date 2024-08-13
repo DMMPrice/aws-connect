71290834ab7dfd78bede464d430c27574ce3e33c
Aniruddha Ghosh <ghoshaniruddha2003@gmail.com>
2024-08-13 12:27:48 +0530

    reformat the port

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.set("views", path.join(__dirname, "views"));
 // Use the routes
 app.use("/", userRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}/users`);
 });
